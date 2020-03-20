e1348d4c50007d903a216791454cd6e51770b757
errorgorn <ashleykhoo2@gmail.com>
2020-03-20 20:46:53 +0800

    added cost finding code

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
-import random
+import os
 
 training=[]
 validation=[]
@@ -19,7 +19,7 @@ def test(epoch_num):
         out.append([])
         out[i]=[sign(x) for x in weights[i]]
 
-    file=open("epoch"+str(epoch_num)+".txt","w")
+    file=open(os.getcwd()+"\\outputs\\epoch"+str(epoch_num)+".txt","w")
     for i in range(150):
         for j in range(51):
             file.write(str(out[i][j])+" ")
@@ -29,7 +29,6 @@ def test(epoch_num):
 
     correct=0
 
-    print(len(validation))
     for k in range(len(validation)):
         layer1=[0]*150
 
@@ -77,13 +76,37 @@ def main():
         for j in range(51):
             weights[i].append(0) #initalizing weights to all 0
 
-    test(0)
+    #test(0)
 
-    #weight will be in the range [-1,1]
     #for each output node the goal is to reach 15 or -15 depending on answer
 
-    BATCH_SIZE=100 #perhaps this is a good number
+    BATCH_SIZE=1000 #make this divisible by 50000
+    GRADIENT_MULTIPLIER=1
+    
+    for epoch_num in range(1,100):
+        for batch in range(len(training)//BATCH_SIZE):
+            cost=[0]*150
+            input_avg=[0]*51
+            
+            for k in range(BATCH_SIZE*batch,BATCH_SIZE*(batch+1)):
+                for i in range(51):
+                    input_avg[i]+=training[k][i]
+                
+                out=[0]*150
+                for i in range(150):
+                    for j in range(51):
+                        out[i]+=weights[i][j]*training[k][j]
+                
+                for i in range(150):
+                    if (i//15==training[k][51]): #correct answer
+                        cost[i]+=(15-out[i])**2
+                    else:
+                        cost[i]+=(out[i]+15)**2
 
+            print(cost)
+            print(input_avg)
+        test(epoch_num)
+        
 
 if __name__=="__main__":
     main()
